0068ea708f71364db6071c448588c5d483c51d44
rmazheika <r.mazheika@andersenlab.com>
2017-07-20 18:32:05 +0300

    Added login UI

--- app/src/main/java/uk/co/ribot/androidboilerplate/ui/login/LoginActivity.java
+++ app/src/main/java/uk/co/ribot/androidboilerplate/ui/login/LoginActivity.java
@@ -1,12 +1,45 @@
 package uk.co.ribot.androidboilerplate.ui.login;
 
+import android.os.Bundle;
+
+import javax.inject.Inject;
+
+import butterknife.ButterKnife;
+import uk.co.ribot.androidboilerplate.R;
 import uk.co.ribot.androidboilerplate.ui.base.BaseActivity;
 
 /**
  * Created by anduser on 20.07.17.
  */
 
-public class LoginActivity extends BaseActivity {
+public class LoginActivity extends BaseActivity implements LoginMvpView {
 
+    @Inject LoginPresenter loginPresenter;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activityComponent().inject(this);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        loginPresenter.attachView(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        loginPresenter.detachView();
+    }
+
+    @Override
+    public void showLoginSuccess() {
+
+    }
+
+    @Override
+    public void showLoginError() {
+
+    }
 }
--- app/src/main/java/uk/co/ribot/androidboilerplate/ui/login/LoginPresenter.java
+++ app/src/main/java/uk/co/ribot/androidboilerplate/ui/login/LoginPresenter.java
@@ -1,5 +1,8 @@
 package uk.co.ribot.androidboilerplate.ui.login;
 
+import javax.inject.Inject;
+
+import rx.Subscription;
 import uk.co.ribot.androidboilerplate.injection.ConfigPersistent;
 import uk.co.ribot.androidboilerplate.ui.base.BasePresenter;
 
@@ -10,5 +13,22 @@ import uk.co.ribot.androidboilerplate.ui.base.BasePresenter;
 @ConfigPersistent
 public class LoginPresenter extends BasePresenter<LoginMvpView> {
 
+    private Subscription mSubscription;
 
+    @Inject LoginPresenter() {};
+
+    @Override
+    public void attachView(LoginMvpView loginMvpView) {
+        super.attachView(loginMvpView);
+    }
+
+    @Override
+    public void detachView() {
+        super.detachView();
+        if (mSubscription != null) mSubscription.unsubscribe();
+    }
+
+    public void executeLoginRequest(String login, String password) {
+
+    }
 }
