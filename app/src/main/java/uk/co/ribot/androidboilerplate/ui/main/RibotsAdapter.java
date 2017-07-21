81eb4a4545098224a5f6751a14a7942f7fcbf9b9
rmazheika <r.mazheika@andersenlab.com>
2017-07-21 17:56:55 +0300

    Added retrolamda and on card click

--- app/src/main/java/uk/co/ribot/androidboilerplate/ui/main/RibotsAdapter.java
+++ app/src/main/java/uk/co/ribot/androidboilerplate/ui/main/RibotsAdapter.java
@@ -1,6 +1,11 @@
 package uk.co.ribot.androidboilerplate.ui.main;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
 import android.graphics.Color;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,16 +19,20 @@ import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import uk.co.ribot.androidboilerplate.R;
 import uk.co.ribot.androidboilerplate.data.model.Ribot;
+import uk.co.ribot.androidboilerplate.util.DialogFactory;
 
 public class RibotsAdapter extends RecyclerView.Adapter<RibotsAdapter.RibotViewHolder> {
 
     private List<Ribot> mRibots;
+    private Context context;
 
     @Inject
-    public RibotsAdapter() {
+    public RibotsAdapter(Activity activity) {
         mRibots = new ArrayList<>();
+        context = activity;
     }
 
     public void setRibots(List<Ribot> ribots) {
@@ -56,6 +65,12 @@ public class RibotsAdapter extends RecyclerView.Adapter<RibotsAdapter.RibotViewH
         @BindView(R.id.view_hex_color) View hexColorView;
         @BindView(R.id.text_name) TextView nameTextView;
         @BindView(R.id.text_email) TextView emailTextView;
+
+        @OnClick(R.id.card_view)
+        public void onCardClick() {
+            DialogFactory.createSimpleOkErrorDialog(context, "Ribot",
+                    mRibots.get(getAdapterPosition()).profile().name().first()).show();
+        }
 
         public RibotViewHolder(View itemView) {
             super(itemView);
