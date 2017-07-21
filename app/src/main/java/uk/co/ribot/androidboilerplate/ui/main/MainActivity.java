2e4dd89bbc24cd08ac21eeeef03bf83f6accbda5
rmazheika <r.mazheika@andersenlab.com>
2017-07-21 17:24:54 +0300

    Added floating acction btn, bottom sheet and coordinator layout

--- app/src/main/java/uk/co/ribot/androidboilerplate/ui/main/MainActivity.java
+++ app/src/main/java/uk/co/ribot/androidboilerplate/ui/main/MainActivity.java
@@ -3,8 +3,13 @@ package uk.co.ribot.androidboilerplate.ui.main;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import java.util.Collections;
@@ -14,6 +19,7 @@ import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import uk.co.ribot.androidboilerplate.R;
 import uk.co.ribot.androidboilerplate.data.SyncService;
 import uk.co.ribot.androidboilerplate.data.model.Ribot;
@@ -25,10 +31,17 @@ public class MainActivity extends BaseActivity implements MainMvpView {
     private static final String EXTRA_TRIGGER_SYNC_FLAG =
             "uk.co.ribot.androidboilerplate.ui.main.MainActivity.EXTRA_TRIGGER_SYNC_FLAG";
 
+    private BottomSheetBehavior bottomSheetBehavior;
+
     @Inject MainPresenter mMainPresenter;
     @Inject RibotsAdapter mRibotsAdapter;
 
-    @BindView(R.id.recycler_view) RecyclerView mRecyclerView;
+    @BindView(R.id.recycler_view)
+    RecyclerView mRecyclerView;
+    @BindView(R.id.bottom_sheet)
+    LinearLayout bottomSheet;
+    @BindView(R.id.btn_bottom_sheet)
+    FloatingActionButton floatingActionButton;
 
     /**
      * Return an Intent to start this Activity.
@@ -48,6 +61,8 @@ public class MainActivity extends BaseActivity implements MainMvpView {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+
         mRecyclerView.setAdapter(mRibotsAdapter);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mMainPresenter.attachView(this);
@@ -56,6 +71,19 @@ public class MainActivity extends BaseActivity implements MainMvpView {
         if (getIntent().getBooleanExtra(EXTRA_TRIGGER_SYNC_FLAG, true)) {
             startService(SyncService.getStartIntent(this));
         }
+
+        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                floatingActionButton.animate().scaleX(1 - slideOffset)
+                        .scaleY(1 - slideOffset).setDuration(0).start();
+            }
+        });
     }
 
     @Override
@@ -63,6 +91,11 @@ public class MainActivity extends BaseActivity implements MainMvpView {
         super.onDestroy();
 
         mMainPresenter.detachView();
+    }
+
+    @OnClick(R.id.btn_bottom_sheet)
+    public void onFabClick() {
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
     }
 
     /***** MVP View methods implementation *****/
@@ -85,5 +118,4 @@ public class MainActivity extends BaseActivity implements MainMvpView {
         mRibotsAdapter.notifyDataSetChanged();
         Toast.makeText(this, R.string.empty_ribots, Toast.LENGTH_LONG).show();
     }
-
 }
